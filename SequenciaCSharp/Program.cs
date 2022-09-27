b64366851bc4c1dd99320edf272d4c80eba092e7
Anderson Lomba <andersonlomba172@outlook.com>
2022-09-27 13:59:55 -0300

    Add project files.

---
+++ SequenciaCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Digite um número por favor:");
+        int num = int.Parse(System.Console.ReadLine());
+        int contador = 0;
+        while (num > 1)
+        {
+
+            contador++;
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+                System.Console.WriteLine(num);
+            }
+            else
+            {
+
+                num = (num * 3) + 1;
+            }
+            Console.WriteLine("Número de voltas da sequência {0}. Atual número da hipótese {1}", contador, num);
+        }
+
+    }
+}
